eeb2a901e76fcf17ecee6080154b746fcc109edb
Christofero Pollano <Dino@Christofero-Pollanos-MacBook-Pro.local>
2013-10-02 14:11:17 +0100

    when running, an audio file will play to test the patches, file is located in the source folder

---
+++ Source/SeriesDeviceCallBacks.cpp
@@ -0,0 +1,69 @@
+/*
+  ==============================================================================
+
+    SeriesDeviceCallBacks.cpp
+    Created: 1 Oct 2013 4:23:29pm
+    Author:  Christofero Pollano
+
+  ==============================================================================
+*/
+
+#include "SeriesDeviceCallBacks.h"
+
+SeriesDeviceCallBacks:: SeriesDeviceCallBacks()
+: source(NULL), buffer(NULL) {
+    processor.setProcessor(&stompbox);
+}
+
+SeriesDeviceCallBacks::~SeriesDeviceCallBacks(){
+    if(buffer != NULL){
+        for(int i=0; i<channels; ++i)
+            delete buffer[i];
+        delete buffer;
+    }
+}
+
+StompBoxAudioProcessor& SeriesDeviceCallBacks::getStompbox(){
+    return stompbox;
+}
+
+void 	SeriesDeviceCallBacks::audioDeviceIOCallback (const float **inputChannelData, int numInputChannels, float **outputChannelData, int numOutputChannels, int numSamples)
+{
+    player.audioDeviceIOCallback(inputChannelData, numInputChannels,
+                                buffer, channels, samples);
+    
+    processor.audioDeviceIOCallback((const float**)buffer, channels, outputChannelData,
+                                    numOutputChannels, samples);
+    
+}
+
+void SeriesDeviceCallBacks::setInputFile(File input){
+    FileInputStream* stream = input.createInputStream();
+    CoreAudioFormat format;
+    source = new AudioFormatReaderSource(format.createReaderFor(stream, false), true);
+    player.setSource(source);
+}
+
+
+void SeriesDeviceCallBacks::audioDeviceAboutToStart (AudioIODevice *device){
+    channels = device->getActiveOutputChannels().toInteger();
+    samples = device->getCurrentBufferSizeSamples();
+    buffer = new float * [channels];
+    for(int i=0; i<channels; ++i)
+        buffer[i] = new float[samples];
+    processor.audioDeviceAboutToStart(device);
+    player.audioDeviceAboutToStart(device);
+}
+
+void 	SeriesDeviceCallBacks::audioDeviceStopped (){
+    processor.audioDeviceStopped();
+    player.audioDeviceStopped();
+}
+
+/*
+SeriesDeviceCallBacks :: SeriesDeviceCallBacks
+
+: AudioIODeviceCallback
+
+
+*/
---
+++ Source/SeriesDeviceCallBacks.h
@@ -0,0 +1,49 @@
+/*
+  ==============================================================================
+
+    SeriesDeviceCallBacks.h
+    Created: 1 Oct 2013 4:23:29pm
+    Author:  Christofero Pollano
+
+  ==============================================================================
+*/
+
+#ifndef SERIESDEVICECALLBACKS_H_INCLUDED
+#define SERIESDEVICECALLBACKS_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+class SeriesDeviceCallBacks : public AudioIODeviceCallback
+{
+    
+private:
+    AudioProcessorPlayer processor;
+    AudioSourcePlayer player;
+    ScopedPointer<AudioFormatReaderSource> source;
+    StompBoxAudioProcessor stompbox;
+
+    float** buffer;
+    int channels, samples;
+public:
+    
+    SeriesDeviceCallBacks();
+    ~SeriesDeviceCallBacks();
+    
+    virtual void 	audioDeviceIOCallback (const float **inputChannelData, int numInputChannels, float **outputChannelData, int numOutputChannels, int numSamples);
+    
+    virtual void 	audioDeviceAboutToStart (AudioIODevice *device);
+    
+    virtual void 	audioDeviceStopped ();
+
+    StompBoxAudioProcessor& getStompbox();
+    
+    void setInputFile(File input);
+//    void sourcePlayerCallBack();
+    
+//    void processorPlayerCallBack();
+};
+
+
+
+#endif  // SERIESDEVICECALLBACKS_H_INCLUDED
