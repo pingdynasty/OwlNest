7a316097b4937fd9eb6097075f27ea07e150e711
dino <cpollano@hotmail.co.uk>
2013-10-24 16:50:08 +0100

    Made an enum header file to help with configs and patch changes

---
+++ Source/Enums.h
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    Enums.h
+    Created: 24 Oct 2013 3:53:42pm
+    Author:  Christofero Pollano
+
+  ==============================================================================
+*/
+
+#ifndef ENUMS_H_INCLUDED
+#define ENUMS_H_INCLUDED
+
+enum  ConfigModes { SINGLE = 1,DUAL = 2,SERIES = 3,PARALLEL = 4};
+
+enum PatchModes { A = 0, B = 1};
+
+
+#endif  // ENUMS_H_INCLUDED
